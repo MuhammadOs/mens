0c0acfbfb60fe0d2309e2a0e3830490a4c0bf525
Muhammad Osama <muhammad2372002@gmail.com>
2026-02-06 04:30:58 +0200

    continue_as_guest_commit

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,6 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
   final prefs = await SharedPreferences.getInstance();
   final languageCode = prefs.getString('languageCode') ?? 'en';
   final initialLocale = Locale(languageCode);
